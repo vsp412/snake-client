51322acd1b958ebf9c6d4c913f5d6d810f3a0f4c
Vineet Pande <vsp4991@gmail.com>
2021-02-11 09:23:21 +0000

    snek3

--- play.js
+++ play.js
@@ -8,3 +8,28 @@ const  connect  = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const handleUserInput = (s) => {
+
+  s.on('data', (k) => {
+    if (k === '\u0003') {
+      process.exit();
+    }
+  });
+
+}
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput(stdin);
+  return stdin;
+}
+setupInput();
+

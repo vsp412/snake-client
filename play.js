e37592d45c4d26bf8b529e8888d0f07a59592d9b
Vineet Pande <vsp4991@gmail.com>
2021-02-11 08:41:11 +0000

    snek2done

--- play.js
+++ play.js
@@ -1,22 +1,10 @@
 const net = require('net');
+const  connect  = require('./client');
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
 
-  return conn;
-}
 
 console.log('Connecting ...');
 connect();
